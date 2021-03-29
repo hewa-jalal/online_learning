4bffd25a412e9b288e30db81e3a1f911db943521
hiwa-jalal <hiwadoski21@gmail.com>
2021-03-29 09:11:10 +0300

    improved lecture expansion tile

--- lib/features/lectures/presentation/UI/widgets/lecture_card.dart
+++ lib/features/lectures/presentation/UI/widgets/lecture_card.dart
@@ -1,4 +1,5 @@
 import 'package:circular_check_box/circular_check_box.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
 import 'package:online_learning/features/user/presentation/bloc/user_auth_bloc.dart';
 
@@ -22,16 +23,25 @@ class LectureCard extends StatelessWidget {
     var isSubmitted =
         lecture.submittedUsers.contains(_userAuthState.id.toString());
 
-    return ExpansionTile(
-      childrenPadding: EdgeInsets.only(left: 8.0, right: 8.0),
+    return ExpansionTileCard(
+      contentPadding: EdgeInsets.only(left: 8.0, right: 8.0),
+      baseColor: Colors.transparent,
+      expandedTextColor: Color(0xff5F36DA),
+      expandedColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      // childrenPadding: EdgeInsets.only(left: 8.0, right: 8.0),
       title: Text(lecture.title),
       children: [
-        SelectableText(
-          'dolor sit amet, consectetur adipiscing elit. Quisque quis congue metus, ac tempus eros. Nunc tincidunt eros arcu, ac dictum nulla convallis sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus commodo pretium leo, ac dictum lacus pellentesque quis. Fusce suscipit, orci eget venenatis dignissim, purus turpis imperdiet velit, vitae ullamcorper lectus libero eget eros.',
+        Padding(
+          padding: EdgeInsets.only(left: 8.0, right: 8.0),
+          child: SelectableText(
+            'dolor sit amet, consectetur adipiscing elit. Quisque quis congue metus, ac tempus eros. Nunc tincidunt eros arcu, ac dictum nulla convallis sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus commodo pretium leo, ac dictum lacus pellentesque quis. Fusce suscipit, orci eget venenatis dignissim, purus turpis imperdiet velit, vitae ullamcorper lectus libero eget eros.',
+          ),
         ),
         ListTile(
           hoverColor: Colors.red,
-          leading: Icon(Icons.picture_as_pdf_outlined),
+          leading:
+              Icon(Icons.picture_as_pdf_outlined, color: Color(0xff5F36DA)),
           title: Row(
             children: [
               Text('file'),
